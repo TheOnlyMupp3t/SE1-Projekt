9d01711acf3e55f763f5f0c59dfea88e42610b0e
tjarbo <16938041+tjarbo@users.noreply.github.com>
2020-11-25 22:34:47 +0100

    :sparkles: Added api class - needs comments and code exeptions

--- Datenübertragung/blueprint/resources/api.py
+++ Datenübertragung/blueprint/resources/api.py
@@ -1,46 +1,41 @@
-from config import config
-import urllib.request
+import json
+from .config import config
+from http.client import HTTPConnection, HTTPSConnection
+from base64 import b64encode
 from enum import Enum
-
 
 class ApiAbfrage():
     def __init__(self):
         self.host = config['DEFAULT']['host']
         self.port = config['DEFAULT']['port']
+        self.protocol = config['DEFAULT']['protocol']
         self.username = config['DEFAULT']['username']
         self.password = config['DEFAULT']['password']
 
-        print(self.host)
-        print(self.port)
-        print(self.username)
-        print(self.password)
-
-        self.passwdmanager = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-        self.passwdmanager.add_password(
-            None, 'http://asm.fl.dlr.de:10001', 'tinf19cs', '$sse1%8Dh2bw')
-
-        handler = urllib.request.HTTPBasicAuthHandler(self.passwdmanager)
-        opener = urllib.request.build_opener(handler)
-
-        urllib.request.install_opener(opener)
-
-        with opener.open('http://asm.fl.dlr.de:10001/it') as url:
-            data = json.loads(url.read().decode())
-            print(data)
-
     def get(self, endpoint):
-        # with urllib.request.urlopen(self.host + ':' + self.port + '/' + str(endpoint)) as url:
-        with urllib.request.urlopen('http://asm.fl.dlr.de:10001/it') as url:
-            data = json.loads(url.read().decode())
-            print(data)
 
+        if (self.protocol == "http"):
+            c = HTTPConnection(self.host, int(self.port))
+        else:
+            c = HTTPSConnection(self.host, int(self.port))
+        
+        userAndPass = b64encode(bytes(self.username + ':' + self.password, 'utf-8')).decode("ascii")
+        headers = { 'Authorization' : 'Basic %s' %  userAndPass }
+        c.request('GET', endpoint.value, headers=headers)        
+        res = c.getresponse()
+
+        if (res.getcode() == 200):
+            return json.loads(res.read().decode())
+        return None
 
 class Endpoints(Enum):
-    IT = 'it'
-    TERMINAL = 'terminal'
-    RADAR = 'radar'
-    FLIGHTPLANS = 'flightplans'
+    IT = '/it'
+    TERMINAL = '/terminal'
+    RADAR = '/radar'
+    FLIGHTPLANS = '/flightplans'
 
 
 api = ApiAbfrage()
-api.get(Endpoints.IT)
+print(api.get(Endpoints.IT))
+print(api.get(Endpoints.FLIGHTPLANS))
+print(api.get(Endpoints.IT))
